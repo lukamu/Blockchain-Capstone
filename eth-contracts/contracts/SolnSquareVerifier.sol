a5182c090d15ba78c2794aa666c090d3a61ed943
Luca <lukamusso@gmail.com>
2019-06-28 17:53:11 -0500

    feat: adding new contracts

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,59 +1,69 @@
 pragma solidity >=0.4.21 <0.6.0;
 
+import './ERC721Mintable.sol';
+
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+import './Verifier.sol';
 
+contract SquareVerifier is Verifier {
 
+}
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+contract SolnSquareVerifier is RealEstateERC721Token {
 
+    SquareVerifier private _verifierContract;
 
+    constructor (address verifierAddress) RealEstateERC721Token() public 
+    {
+            _verifierContract = SquareVerifier(verifierAddress);
+    }
+    
 
 // TODO define a solutions struct that can hold an index & an address
+    struct Solutions {
+        uint tokenId;
+        address solution_address;
+    }
 
 
 // TODO define an array of the above struct
+    Solutions[] solutionsArray;
 
 
 // TODO define a mapping to store unique solutions submitted
-
+    mapping (bytes32 => Solutions) private uniqueSolutions;
 
 
 // TODO Create an event to emit when a solution is added
-
+    event SolutionAdded (uint tokenid, address solution_address);
 
 
 // TODO Create a function to add the solutions to the array and emit the event
-
+    function AddSolution(address _solution_address, uint256 _tokenId, bytes32 key) public {
+        Solutions memory solution = Solutions({ tokenId :_tokenId, solution_address:_solution_address });
+        solutionsArray.push(solution);
+        uniqueSolutions[key] = solution;
+        emit SolutionAdded(_tokenId, _solution_address);
+    }
 
 
 // TODO Create a function to mint new NFT only after the solution has been verified
 //  - make sure the solution is unique (has not been used before)
 //  - make sure you handle metadata as well as tokenSuplly
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    function mintNFT (address _solution_address, uint _tokenId,
+    // passing on the 9 parameters required for zokrates verification
+            uint[2] memory a, uint[2] memory a_p,
+            uint[2][2] memory b, uint[2] memory b_p,
+            uint[2] memory c, uint[2] memory c_p,
+            uint[2] memory h, uint[2] memory k,
+            uint[2] memory input) public
+    {
+        bytes32 key = keccak256(abi.encodePacked(a, a_p, b, b_p, c, c_p, h, k, input));
+        require(uniqueSolutions[key].solution_address == address(0), "Solution already used");
+        require(_verifierContract.verifyTx (a, a_p, b, b_p, c, c_p, h, k, input)," solution not verified");
+        
+        AddSolution(_solution_address, _tokenId, key);
+        super.mint(_solution_address, _tokenId);
+    } 
+}
---
+++ eth-contracts/contracts/Verifier.sol
@@ -0,0 +1,235 @@
+// This file is MIT Licensed.
+//
+// Copyright 2017 Christian Reitwiessner
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+pragma solidity >=0.4.21 <0.6.0;
+
+library Pairing {
+    struct G1Point {
+        uint X;
+        uint Y;
+    }
+    // Encoding of field elements is: X[0] * z + X[1]
+    struct G2Point {
+        uint[2] X;
+        uint[2] Y;
+    }
+    /// @return the generator of G1
+    function P1() pure internal returns (G1Point memory) {
+        return G1Point(1, 2);
+    }
+    /// @return the generator of G2
+    function P2() pure internal returns (G2Point memory) {
+        return G2Point (
+            [11559732032986387107991004021392285783925812861821192530917403151452391805634,
+             10857046999023057135944570762232829481370756359578518086990519993285655852781],
+            [4082367875863433681332203403145435568316851327593401208105741076214120093531,
+             8495653923123431417604973247489272438418190587263600148770280649306958101930]
+        );
+    }
+    /// @return the negation of p, i.e. p.addition(p.negate()) should be zero.
+    function negate(G1Point memory p) pure internal returns (G1Point memory) {
+        // The prime q in the base field F_q for G1
+        uint q = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
+        if (p.X == 0 && p.Y == 0)
+            return G1Point(0, 0);
+        return G1Point(p.X, q - (p.Y % q));
+    }
+    /// @return the sum of two points of G1
+    function addition(G1Point memory p1, G1Point memory p2) internal returns (G1Point memory r) {
+        uint[4] memory input;
+        input[0] = p1.X;
+        input[1] = p1.Y;
+        input[2] = p2.X;
+        input[3] = p2.Y;
+        bool success;
+        assembly {
+            success := call(sub(gas, 2000), 6, 0, input, 0xc0, r, 0x60)
+            // Use "invalid" to make gas estimation work
+            switch success case 0 { invalid() }
+        }
+        require(success);
+    }
+    /// @return the product of a point on G1 and a scalar, i.e.
+    /// p == p.scalar_mul(1) and p.addition(p) == p.scalar_mul(2) for all points p.
+    function scalar_mul(G1Point memory p, uint s) internal returns (G1Point memory r) {
+        uint[3] memory input;
+        input[0] = p.X;
+        input[1] = p.Y;
+        input[2] = s;
+        bool success;
+        assembly {
+            success := call(sub(gas, 2000), 7, 0, input, 0x80, r, 0x60)
+            // Use "invalid" to make gas estimation work
+            switch success case 0 { invalid() }
+        }
+        require (success);
+    }
+    /// @return the result of computing the pairing check
+    /// e(p1[0], p2[0]) *  .... * e(p1[n], p2[n]) == 1
+    /// For example pairing([P1(), P1().negate()], [P2(), P2()]) should
+    /// return true.
+    function pairing(G1Point[] memory p1, G2Point[] memory p2) internal returns (bool) {
+        require(p1.length == p2.length);
+        uint elements = p1.length;
+        uint inputSize = elements * 6;
+        uint[] memory input = new uint[](inputSize);
+        for (uint i = 0; i < elements; i++)
+        {
+            input[i * 6 + 0] = p1[i].X;
+            input[i * 6 + 1] = p1[i].Y;
+            input[i * 6 + 2] = p2[i].X[0];
+            input[i * 6 + 3] = p2[i].X[1];
+            input[i * 6 + 4] = p2[i].Y[0];
+            input[i * 6 + 5] = p2[i].Y[1];
+        }
+        uint[1] memory out;
+        bool success;
+        assembly {
+            success := call(sub(gas, 2000), 8, 0, add(input, 0x20), mul(inputSize, 0x20), out, 0x20)
+            // Use "invalid" to make gas estimation work
+            switch success case 0 { invalid() }
+        }
+        require(success);
+        return out[0] != 0;
+    }
+    /// Convenience method for a pairing check for two pairs.
+    function pairingProd2(G1Point memory a1, G2Point memory a2, G1Point memory b1, G2Point memory b2) internal returns (bool) {
+        G1Point[] memory p1 = new G1Point[](2);
+        G2Point[] memory p2 = new G2Point[] (2);
+        p1[0] = a1;
+        p1[1] = b1;
+        p2[0] = a2;
+        p2[1] = b2;
+        return pairing(p1, p2);
+    }
+    /// Convenience method for a pairing check for three pairs.
+    function pairingProd3(
+            G1Point memory a1, G2Point memory a2,
+            G1Point memory b1, G2Point memory b2,
+            G1Point memory c1, G2Point memory c2
+    ) internal returns (bool) {
+        G1Point[] memory p1 = new G1Point[](3);
+        G2Point[] memory p2 = new G2Point[] (3);
+        p1[0] = a1;
+        p1[1] = b1;
+        p1[2] = c1;
+        p2[0] = a2;
+        p2[1] = b2;
+        p2[2] = c2;
+        return pairing(p1, p2);
+    }
+    /// Convenience method for a pairing check for four pairs.
+    function pairingProd4(
+            G1Point memory a1, G2Point memory a2,
+            G1Point memory b1, G2Point memory b2,
+            G1Point memory c1, G2Point memory c2,
+            G1Point memory d1, G2Point memory d2
+    ) internal returns (bool) {
+        G1Point[] memory p1 = new G1Point[](4);
+        G2Point[] memory p2 = new G2Point[](4);
+        p1[0] = a1;
+        p1[1] = b1;
+        p1[2] = c1;
+        p1[3] = d1;
+        p2[0] = a2;
+        p2[1] = b2;
+        p2[2] = c2;
+        p2[3] = d2;
+        return pairing(p1, p2);
+    }
+}
+contract Verifier {
+    using Pairing for *;
+    struct VerifyingKey {
+        Pairing.G2Point A;
+        Pairing.G1Point B;
+        Pairing.G2Point C;
+        Pairing.G2Point gamma;
+        Pairing.G1Point gammaBeta1;
+        Pairing.G2Point gammaBeta2;
+        Pairing.G2Point Z;
+        Pairing.G1Point[] IC;
+    }
+    struct Proof {
+        Pairing.G1Point A;
+        Pairing.G1Point A_p;
+        Pairing.G2Point B;
+        Pairing.G1Point B_p;
+        Pairing.G1Point C;
+        Pairing.G1Point C_p;
+        Pairing.G1Point K;
+        Pairing.G1Point H;
+    }
+    function verifyingKey() pure internal returns (VerifyingKey memory vk) {
+        vk.A = Pairing.G2Point ([0x8124b55951417df1264c2d7105b06a96610f136f040be7f0d516a1cc197bc08, 0xf2d9e40dfcab9bcd007b447a2b0281b65268fad0779ab273800218786350c17], [0xa8d2d9c13feeede3e83df9ca23a7508f69d8438af1749f8d66d20ca05965e04, 0x21dc247bea7996eee0d790669031fb1c99e060732e0f631963dd0967fc82c9d]);
+        vk.B = Pairing.G1Point(0x1962bd8ea83c38ade65a880e3b92be4602a82c2cc2a7de7b37108532b5ac6a28, 0x32d73ced9cbaf43122cfbc115929f8afae44285e8492114ebd6c7f8bd756d2c);
+        vk.C = Pairing.G2Point ([0x56dfb23a752bf1eeef07f132c4acad2993207bb2a4664f468fd3507fa75f320, 0x210363f5ad2b8f873059e0becf19eaebf7eb890bc808094756a72c15762ccf40], [0x1292348594fe3f187ca634a993c2433bdb5fe47886000cef761fbb2e9fdd271e, 0x165aef9b664ed46f4ae7d8d529549fb7817f45e81d87df365e3bbcf36b38db1b]);
+        vk.gamma = Pairing.G2Point ([0x156b87d8d5a6fc46083a6af4708bb8aa30c15c4b35409464e69515b4eb0fe733, 0xd5704927cc43a46e30b26c88fe139cc33460da8b9a086b21dcdbee28d913ef6], [0x1b04ca2c3f525058d0664eddb16050c60b6268338cbe8af7765c540e058cd853, 0x6c0a9d382d7b0018bf2493a0ca0268415b6fffcfcc30e18377e4afcfcb1af93]);
+        vk.gammaBeta1 = Pairing.G1Point(0x599da502854500cdc174a8afb685f0493ee4f7c0bc303c9779e0d381f7b1b36, 0xd6baa39c6e62ab7562e488718f67da20f0545301e9ebf002e52ca18e7652651);
+        vk.gammaBeta2 = Pairing.G2Point ([0x10b6490bde8f971895a0b007be54447f099cdac04e19400943bb3d1bec6d39e5, 0x30614d554f5f9e2d7a8b6bdff5c337d916fa755fac3257a6fb9a8347f6031e7e], [0xa0b79a703465bf36de3908d16bac6edcbade451f14ee07ee00948f442acb4d7, 0x17e0ccef1857e7384945a86826bd73921e40df25bdaa394f92b29504441e3b96]);
+        vk.Z = Pairing.G2Point ([0x14eb43f5a1a0d3664c1e239171cb8490eac7b1e23c39ae40329f3c78df0b658f, 0x1bea2ed30b49fba5c7f3759810759fc5e811ad0aab5666a5b1c25a49b3c9f5b9], [0x20d3dd213703d76a6e9c9cf53412f88a9db5b977aa7ef12602701c01ea26ece3, 0x2890545de2fdefc94c515816318ff98d620210757b8fd315c20b7ba5f00d2b28]);
+        vk.IC = new Pairing.G1Point[](3);
+        vk.IC[0] = Pairing.G1Point(0x3f9de852468b7642b19c3f2b7826545ab56fa54e0045027e4b97aa98e29c05e, 0x2d0232c57ace046a40ec7956990dc0e562e159dff859bc3efe94008198b3dfe6);
+        vk.IC[1] = Pairing.G1Point(0x97813858ac3a81207524c3a4ce8f8764e6dfd81cc4a3f1521ac268d8283bca9, 0x2b337f9893922b6f5a201b58f29af07ff87dfea9577157f1f0496074be937721);
+        vk.IC[2] = Pairing.G1Point(0x303788ebc574ff55c77f0658d0001ae9095df08085074b46f06adfd363d484bc, 0x292df8cf3fab769f476e6e6ed3d094dee464c625fe1ce15b31f4c990dd9d9d5d);
+    }
+    function verify(uint[] memory input, Proof memory proof) internal returns (uint) {
+        VerifyingKey memory vk = verifyingKey();
+        require(input.length + 1 == vk.IC.length);
+        // Compute the linear combination vk_x
+        Pairing.G1Point memory vk_x = Pairing.G1Point(0, 0);
+        for (uint i = 0; i < input.length; i++)
+            vk_x = Pairing.addition(vk_x, Pairing.scalar_mul(vk.IC[i + 1], input[i]));
+        vk_x = Pairing.addition(vk_x, vk.IC[0]);
+        if (!Pairing.pairingProd2(proof.A, vk.A, Pairing.negate(proof.A_p), Pairing.P2())) return 1;
+        if (!Pairing.pairingProd2(vk.B, proof.B, Pairing.negate(proof.B_p), Pairing.P2())) return 2;
+        if (!Pairing.pairingProd2(proof.C, vk.C, Pairing.negate(proof.C_p), Pairing.P2())) return 3;
+        if (!Pairing.pairingProd3(
+            proof.K, vk.gamma,
+            Pairing.negate(Pairing.addition(vk_x, Pairing.addition(proof.A, proof.C))), vk.gammaBeta2,
+            Pairing.negate(vk.gammaBeta1), proof.B
+        )) return 4;
+        if (!Pairing.pairingProd3(
+                Pairing.addition(vk_x, proof.A), proof.B,
+                Pairing.negate(proof.H), vk.Z,
+                Pairing.negate(proof.C), Pairing.P2()
+        )) return 5;
+        return 0;
+    }
+    event Verified(string s);
+    function verifyTx(
+            uint[2] memory a,
+            uint[2] memory a_p,
+            uint[2][2] memory b,
+            uint[2] memory b_p,
+            uint[2] memory c,
+            uint[2] memory c_p,
+            uint[2] memory h,
+            uint[2] memory k,
+            uint[2] memory input
+        ) public returns (bool r) {
+        Proof memory proof;
+        proof.A = Pairing.G1Point(a[0], a[1]);
+        proof.A_p = Pairing.G1Point(a_p[0], a_p[1]);
+        proof.B = Pairing.G2Point ([b[0][0], b[0][1]], [b[1][0], b[1][1]]);
+        proof.B_p = Pairing.G1Point(b_p[0], b_p[1]);
+        proof.C = Pairing.G1Point(c[0], c[1]);
+        proof.C_p = Pairing.G1Point(c_p[0], c_p[1]);
+        proof.H = Pairing.G1Point(h[0], h[1]);
+        proof.K = Pairing.G1Point(k[0], k[1]);
+        uint[] memory inputValues = new uint[](input.length);
+        for(uint i = 0; i < input.length; i++){
+            inputValues[i] = input[i];
+        }
+        if (verify(inputValues, proof) == 0) {
+            emit Verified("Transaction successfully verified.");
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
